c1b7027e67a9f38332f2fa0b84636b15f9adbb3d
Matthew Butt <matthew.butt@7digital.com>
2011-08-22 17:40:30 +0100

    Added navigability

--- main.js
+++ main.js
@@ -2,26 +2,67 @@
 	$("#go").click(submitRequest);	
 	$("#request").ajaxSend(logRequest);
 	$("#response").ajaxComplete(logResponse);
+	$("#response a").live("click", doGetRequest);
+	$("[name=verb]").change(showRequestBody);
+	$("[name=requestBody]").hide();
 });
 
 function submitRequest(){
 	var verb = $("[name=verb]").val();
 	var url = $("[name=url]").val();
-	$.ajax({
+	var settings = {
 		url: url,
 		type: verb
-	});
+	};
+	var requestBody = $("[name=requestBody]").val();
+	if(requestBody != undefined) settings.data = requestBody;
+	$.ajax(settings);
 }
 
 function logRequest(e, jqXHR, settings){
-	var requestText = settings.type + " " + settings.url + "HTTP/1.1";
-	if(jqXHR.requestText != undefined) requestText += "\n" + jqXHR.requestText;
+	var requestText = settings.type + " " + settings.url + " HTTP/1.1";
+	if(settings.data != undefined) requestText += "\n\n" + settings.data;
 	$(this).text(requestText);
 }
 
-function logResponse(e, jqXHR, textStatus){
-	var responseText = "HTTP/1.1 " + jqXHR.status + " " + textStatus;
+function logResponse(e, jqXHR, settings){
+	var responseText = "HTTP/1.1 " + jqXHR.status;
 	responseText += "\n" + jqXHR.getAllResponseHeaders();
-	if(jqXHR.responseText != undefined) responseText += "\n" + jqXHR.responseText;
+	if(jqXHR.responseText != undefined) responseText += "\n\n" + jqXHR.responseText;
 	$(this).text(responseText);
+	
+	var html = $(this).html();
+	$(this).html(addLinks(html));
+}
+
+function doGetRequest(){
+	var url = this.href;
+	$("[name=url]").val(url);
+	
+	var verb = "GET";
+	$("[name=verb]").val(verb);
+	
+	$.ajax({
+		url: url,
+		verb: verb
+	});
+	return false;
+}
+
+function showRequestBody(){
+	switch($(this).val())
+	{
+		case "PUT":
+		case "POST":
+			$("[name=requestBody]").show();
+			break;
+		default:
+			$("[name=requestBody]").hide();
+			break;
+	}
+}
+
+function addLinks(input){
+	var urlRegex = /https?:\/\/([\S]+\.)+[A-Z]+(\/[a-z0-9\._\/~%\-\+&\#\?!=\(\)@]*)?/gi;	
+	return input.replace(urlRegex, "<a href=\"$&\">$&</a>");
 }
