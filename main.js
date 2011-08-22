bf1ff342db3e57b6b66dec5d4791d930d1ea0652
Matthew Butt <matthew.butt@7digital.com>
2011-08-22 15:49:15 +0100

    Started project

---
+++ main.js
@@ -0,0 +1,26 @@
+﻿$(function(){
+	$("#go").click(submitRequest);	
+});
+
+function submitRequest(){
+	var verb = $("[name=verb]").val();
+	var url = $("[name=url]").val();
+	$.ajax({
+		url: url,
+		type: verb,
+		beforeSend: alogRequest,
+		complete: alogResponse
+	});
+}
+
+function logRequest(jqXHR, settings){
+	var requestText = settings.type + " " + settings.url + "HTTP/1.1";
+	if(jqXHR.requestText != undefined) requestText += jqXHR.requestText;
+	$("#request").text(requestText);
+}
+
+function logResponse(jqXHR, textStatus){
+	var responseText = "HTTP/1.1 " + jqXHR.status + " " + textStatus;
+	responseText += "\n" + jqXHR.getAllResponseHeaders();
+	$("#response").text(responseText);
+}
