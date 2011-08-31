978a1d7bdded22886f23b5229f0107cec517ce60
Matthew Butt <matthew.butt@7digital.com>
2011-08-31 17:51:43 +0100

    JSLint

--- main.js
+++ main.js
@@ -1,70 +1,83 @@
-﻿$(function(){
-	$("#go").click(submitRequest);	
-	$("#request").ajaxSend(logRequest);
-	$("#response").ajaxComplete(logResponse);
-	$("#response a").live("click", doGetRequest);
-	$("[name=verb]").change(showRequestBody);
-	$("[name=requestBody]").hide();
-});
+﻿(function () {
+	"use strict";
 
-function submitRequest(){
-	var verb = $("[name=verb]").val();
-	var url = $("[name=url]").val();
-	var settings = {
-		url: url,
-		type: verb
-	};
-	var requestBody = $("[name=requestBody]").val();
-	if(requestBody != undefined) settings.data = requestBody;
-	$.ajax(settings);
-}
+	var submitRequest = function () {
+			var verb = $("[name=verb]").val(),
+				url = $("[name=url]").val(),
+				settings = {
+					url: url,
+					type: verb
+				},
+				requestBody = $("[name=requestBody]").val();
+			
+			if (requestBody !== undefined) {
+				settings.data = requestBody;
+			}
+			$.ajax(settings);
+		},
 
-function logRequest(e, jqXHR, settings){
-	var requestText = settings.type + " " + settings.url + " HTTP/1.1";
-	if(settings.data != undefined) requestText += "\n\n" + settings.data;
-	$(this).text(requestText);
-}
+		logRequest = function (e, jqXHR, settings) {
+			var requestText = settings.type + " " + settings.url + " HTTP/1.1";
+			if (settings.data !== undefined) {
+				requestText += "\n\n" + settings.data;
+			}
+			$(this).text(requestText);
+		},
 
-function logResponse(e, jqXHR, settings){
-	var responseText = "HTTP/1.1 " + jqXHR.status;
-	responseText += "\n" + jqXHR.getAllResponseHeaders();
-	if(jqXHR.responseText != undefined) responseText += "\n\n" + jqXHR.responseText;
-	$(this).text(responseText);
-	
-	var html = $(this).html();
-	$(this).html(addLinks(html));
-}
+		addLinks = function (input) {
+			var urlRegex = /https?:\/\/([\S]+\.)+[A-Z]+(\/[a-z0-9\._\/~%\-\+&\#\?!=\(\)@]*)?/gi;	
+			return input.replace(urlRegex, "<a href=\"$&\">$&</a>");
+		},
 
-function doGetRequest(){
-	var url = this.href;
-	$("[name=url]").val(url);
-	
-	var verb = "GET";
-	$("[name=verb]").val(verb);
-	
-	$.ajax({
-		url: url,
-		verb: verb
-	});
-	
-	$("[name=requestBody]").hide();
-	return false;
-}
+		logResponse = function (e, jqXHR, settings) {
+			var responseText = "HTTP/1.1 " + jqXHR.status,
+				html;
+			responseText += "\n" + jqXHR.getAllResponseHeaders();
+			if (jqXHR.responseText !== undefined) {
+				responseText += "\n\n" + jqXHR.responseText;
+			}
+			$(this).text(responseText);
+			
+			html = $(this).html();
+			$(this).html(addLinks(html));
+		},
 
-function showRequestBody(){
-	switch($(this).val())
-	{
-		case "PUT":
-		case "POST":
-			$("[name=requestBody]").show();
-			break;
-		default:
+		doGetRequest = function () {
+			var url = this.href,
+				verb = "GET";
+			
+			$("[name=url]").val(url);
+			
+			$("[name=verb]").val(verb);
+			
+			$.ajax({
+				url: url,
+				verb: verb
+			});
+			
 			$("[name=requestBody]").hide();
-			break;
-	}
-}
+			return false;
+		},
 
-function addLinks(input){
-	var urlRegex = /https?:\/\/([\S]+\.)+[A-Z]+(\/[a-z0-9\._\/~%\-\+&\#\?!=\(\)@]*)?/gi;	
-	return input.replace(urlRegex, "<a href=\"$&\">$&</a>");
-}
+		showRequestBody = function () {
+			switch ($(this).val()) {
+			case "PUT":
+			case "POST":
+				$("[name=requestBody]").show();
+				break;
+			default:
+				$("[name=requestBody]").hide();
+				break;
+			}
+		};
+
+	$(function () {
+		$("#go").click(submitRequest);	
+		$("#request").ajaxSend(logRequest);
+		$("#response").ajaxComplete(logResponse);
+		$("#response a").live("click", doGetRequest);
+		$("[name=verb]").change(showRequestBody);
+		$("[name=requestBody]").hide();
+	});
+
+}());
