578310e3638e9dfbec0ea369710ec01a5c523f79
Matthew Butt <matthew.butt@7digital.com>
2011-09-02 12:14:37 +0100

    added more tests

--- app/scripts/hateoas-console/response-parsers/XmlResponseParser.js
+++ app/scripts/hateoas-console/response-parsers/XmlResponseParser.js
@@ -1,4 +1,5 @@
 ﻿/*global HATEOAS_CONSOLE */
+/*jslint regexp: true */
 
 // TODO: decide if there's a better way to namespace
 var HATEOAS_CONSOLE = HATEOAS_CONSOLE || {};
@@ -11,12 +12,28 @@ HATEOAS_CONSOLE.RESPONSE_PARSERS.XmlResponseParser = function () {
 
 HATEOAS_CONSOLE.RESPONSE_PARSERS.XmlResponseParser.prototype.getLinks = function (response) {
 	"use strict";
-	var regex = /(?:href|src|link)="(.*)"/g,
+	var attributeRegex = /(?:href|src|link)="([^"]+)"/g,
+		elementRegex = /<(?:href|src|link)[^>]*>([^<]+)<\/(?:href|src|link)>/g,
+		linksFound = [],
 		links = [],
-		match;
+		match,
+		i,
+		max;
 	
-	while ((match = regex.exec(response))) {
-		links.push(match[1]);
+	while ((match = attributeRegex.exec(response)) !== null) {
+		linksFound.push({index: match.index, link: match[1]});
+	}
+	
+	while ((match = elementRegex.exec(response)) !== null) {
+		linksFound.push({index: match.index, link: match[1]});
+	}
+	
+	linksFound.sort(function (a, b) {
+		return a.index - b.index;
+	});
+	
+	for (i = 0, max = linksFound.length; i < max; i += 1) {
+		links.push(linksFound[i].link);
 	}
 	
 	return links;
--- unit-tests/spec/response-parsers/XmlResponseParserSpec.js
+++ unit-tests/spec/response-parsers/XmlResponseParserSpec.js
@@ -36,5 +36,61 @@ describe("XmlResponseParser", function () {
 			expect(links[0]).toEqual(expectedLink);
 		});
 		
+		it("should identify a URL in an href element", function () {
+			var expectedLink = "http://localhost/bar",
+				links = parser.getLinks("<response><href>" + expectedLink + "</href></response>");
+			
+			expect(links[0]).toEqual(expectedLink);
+		});
+		
+		it("should identify a URL in a src element", function () {
+			var expectedLink = "http://localhost/bar",
+				links = parser.getLinks("<response><src>" + expectedLink + "</src></response>");
+			
+			expect(links[0]).toEqual(expectedLink);
+		});
+		
+		it("should identify a URL in a link element", function () {
+			var expectedLink = "http://localhost/bar",
+				links = parser.getLinks("<response><link>" + expectedLink + "</link></response>");
+			
+			expect(links[0]).toEqual(expectedLink);
+		});
+		
+		it("should identify several URLs when they are present", function () {
+			var expectedLink = "http://localhost/bar",
+				links = parser.getLinks("<response><foo href=\"" + expectedLink + "\" />" +
+						"<bar src=\"" + expectedLink + "1\" />" +
+						"<href>" + expectedLink + "2</href>" +
+						"<src>" + expectedLink + "3</src></response>");
+			
+			expect(links.length).toEqual(4);
+			expect(links[0]).toEqual(expectedLink);
+			expect(links[1]).toEqual(expectedLink + "1");
+			expect(links[2]).toEqual(expectedLink + "2");
+			expect(links[3]).toEqual(expectedLink + "3");
+		});
+		
+		it("should identify several URLs in correct order", function () {
+			var expectedLink = "http://localhost/bar",
+				links = parser.getLinks("<response><foo href=\"" + expectedLink + "\" />" +
+						"<href>" + expectedLink + "1</href>" +
+						"<bar src=\"" + expectedLink + "2\" />" +
+						"<src>" + expectedLink + "3</src></response>");
+			
+			expect(links.length).toEqual(4);
+			expect(links[0]).toEqual(expectedLink);
+			expect(links[1]).toEqual(expectedLink + "1");
+			expect(links[2]).toEqual(expectedLink + "2");
+			expect(links[3]).toEqual(expectedLink + "3");
+		});
+		
+		it("should tolerate additional attributes in containing element", function () {
+			var expectedLink = "http://localhost/bar",
+				links = parser.getLinks("<response><href rel=\"me\">" + expectedLink + "</href></response>");
+				
+			expect(links[0]).toEqual(expectedLink);
+		});
+		
 	});
 });
