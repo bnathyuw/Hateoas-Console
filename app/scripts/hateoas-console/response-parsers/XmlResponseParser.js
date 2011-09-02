2922ad8229c9554d3b47073307b04491dfe6b0c7
Matthew Butt <matthew.butt@7digital.com>
2011-09-02 10:32:58 +0100

    set up initial unit tests in Jasmine

---
+++ app/scripts/hateoas-console/response-parsers/XmlResponseParser.js
@@ -0,0 +1,11 @@
+﻿/*global HATEOAS_CONSOLE */
+var HATEOAS_CONSOLE = HATEOAS_CONSOLE || {};
+HATEOAS_CONSOLE.RESPONSE_PARSERS = HATEOAS_CONSOLE.RESPONSE_PARSERS || {};
+
+HATEOAS_CONSOLE.RESPONSE_PARSERS.XmlResponseParser = function () {
+	"use strict";
+};
+HATEOAS_CONSOLE.RESPONSE_PARSERS.XmlResponseParser.prototype.parse = function (response) {
+	"use strict";
+	return "result";
+};
---
+++ unit-tests/spec/response-parsers/XmlResponseParserSpec.js
@@ -0,0 +1,16 @@
+﻿/*global describe: false, beforeEach: false, it: false, HATEOAS_CONSOLE: false, expect: false */
+describe("XmlResponseParser", function () {
+	"use strict";
+	var parser;
+	
+	beforeEach(function () {
+		parser = new HATEOAS_CONSOLE.RESPONSE_PARSERS.XmlResponseParser();
+	});
+	
+	it("should be able to parse a result", function () {
+		var response = "<response></response>",
+			output = parser.parse(response);
+		
+		expect(typeof output).toEqual("string");
+	});
+});
