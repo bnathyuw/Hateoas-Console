dea2e673962706ed69e19de0a33221bda89afbf9
Matthew Butt <matthew.butt@7digital.com>
2011-09-05 12:05:16 +0100

    Much more refactoring

--- app/scripts/hateoas-console/response-parsers/XmlResponseParser.js
+++ app/scripts/hateoas-console/response-parsers/XmlResponseParser.js
@@ -5,84 +5,64 @@ HATEOAS_CONSOLE.namespace("HATEOAS_CONSOLE.responseParsers");
 
 HATEOAS_CONSOLE.responseParsers.XmlResponseParser = (function () {
 	"use strict";
-	var getLinks,
-		Constr;
 	
-	getLinks = function (response) {
-		var linkInAttributeRegex = /(<[^>]+(?:href|src|link)="([^"]+)"[^>]*>)/g,
-			linkInElementRegex = /(<(?:href|src|link)[^>]*>)([^<]+)<\/(?:href|src|link)>/g,
-			linksFound = [],
-			links = [],
-			link,
-			match,
-			i,
-			addLink = function (location, tag, uri) {
-				var link = {location: location, uri: uri},
-					addAttribute = function (attributeName) {
-						var regex = new RegExp(" " + attributeName + "=\"([^\"]+)\"", "g"),
-							match = regex.exec(tag),
-							values;
-						if (match !== null) {
-							values = match[1].split(" ");
-							link[attributeName] = values;
-						}
-					};
-				
-				addAttribute("rel");
-				addAttribute("rev");
-				
-				linksFound.push(link);
-			},
-			copyDistinctAttributes = function (attributeName, linkFound, link) {
-				link[attributeName] = link[attributeName] || [];
-				
-				if (linkFound[attributeName] === undefined) {
-					return;
+	var findOrCreateLink = function (links, uri) {
+			var link,
+				i;
+			for (i = 0; i < links.length; i += 1) {
+				if (links[i].uri === uri) {
+					return links[i];
 				}
+			}
+			link = {uri: uri, locations: []};
+			links.push(link);
+			return link;
+		},
+		
+		copyDistinctAttributes = function (attributeName, tag, link) {
+			var regex = new RegExp(" " + attributeName + "=\"([^\"]+)\"", "g"),
+				match = regex.exec(tag),
+				values;
+						
+			link[attributeName] = link[attributeName] || [];
 			
-				linkFound[attributeName].forEach(function (value) {
-					if (link[attributeName].indexOf(value) === -1) {
-						link[attributeName].push(value);
-					}
-				});
-			},
-			findOrCreateLink = function (uri) {
-				var link;
-				for (i = 0; i < links.length; i += 1) {
-					if (links[i].uri === uri) {
-						return links[i];
-					}
+			if (match === null) {
+				return;
+			}
+			
+			values = match[1].split(" ");
+			
+			values.forEach(function (value) {
+				if (link[attributeName].indexOf(value) === -1) {
+					link[attributeName].push(value);
 				}
-				link = {uri: uri, locations: []};
-				links.push(link);
-				return link;
-			};
+			});
+		},
 		
-		while ((match = linkInAttributeRegex.exec(response)) !== null) {
-			addLink(match.index, match[1], match[2]);
-		}
-		
-		while ((match = linkInElementRegex.exec(response)) !== null) {
-			addLink(match.index, match[1], match[2]);
-		}
-		
-		linksFound.sort(function (a, b) {
-			return a.location - b.location;
-		});
-		
-		linksFound.forEach(function (linkFound) {
-			var link = findOrCreateLink(linkFound.uri);
+		getLinks = function (response) {
+			var searchRegex = /(<[^>]+(?:href|src|link)="([^"]+)"[^>]*>)|(?:(<(?:href|src|link)[^>]*>)([^<]+)<\/(?:href|src|link)>)/g,
+				links = [],
+				link,
+				match,
+				uri,
+				tag;
 			
-			link.locations.push(linkFound.location);
-					
-			copyDistinctAttributes("rel", linkFound, link);
-			copyDistinctAttributes("rev", linkFound, link);
-		});
+			while ((match = searchRegex.exec(response)) !== null) {
+				tag = match[1] || match[3];
+				uri = match[2] || match[4];
+				
+				link = findOrCreateLink(links, uri);
+				
+				link.locations.push(match.index);
+						
+				copyDistinctAttributes("rel", tag, link);
+				copyDistinctAttributes("rev", tag, link);
+			}
+			
+			return links;
+		},
 		
-		return links;
-	};
-	
-	Constr = function () {};
+		Constr = function () {};
 		
 	Constr.prototype = {
 		constructor: HATEOAS_CONSOLE.responseParsers.XmlResponseParser,
