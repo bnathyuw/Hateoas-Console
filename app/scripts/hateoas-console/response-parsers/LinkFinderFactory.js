f5769dacf597068201543404a356df9a4f0431b5
Matthew Butt <matthew.butt@7digital.com>
2011-09-12 17:39:07 +0100

    JSLint

--- app/scripts/hateoas-console/response-parsers/LinkFinderFactory.js
+++ app/scripts/hateoas-console/response-parsers/LinkFinderFactory.js
@@ -8,26 +8,26 @@ HATEOAS_CONSOLE.namespace("HATEOAS_CONSOLE.responseParsers");
 	var instance,
 		
 		constructors = {
-				xml: HATEOAS_CONSOLE.responseParsers.xmlLinkFinder,
-				json: HATEOAS_CONSOLE.responseParsers.jsonLinkFinder,
-				"json-p": HATEOAS_CONSOLE.responseParsers.jsonpLinkFinder,
-				html: HATEOAS_CONSOLE.responseParsers.xmlLinkFinder
-			},
+			xml: HATEOAS_CONSOLE.responseParsers.xmlLinkFinder,
+			json: HATEOAS_CONSOLE.responseParsers.jsonLinkFinder,
+			"json-p": HATEOAS_CONSOLE.responseParsers.jsonpLinkFinder,
+			html: HATEOAS_CONSOLE.responseParsers.xmlLinkFinder
+		},
 
-			create = function (contentType, spec) {
-				var	mimeType = contentType.split(";").shift(),
-					responseFormat = mimeType.split(/[\/\+]/).pop(),
-					ctor = constructors[responseFormat];
-			
-				if (!ctor) {
-					throw { 
-						name: "Constructor Not Found",
-						message: "No constructor exists for type " + contentType
-					};
-				}
-			
-				return ctor(spec);
-			};
+		create = function (contentType, spec) {
+			var	mimeType = contentType.split(";").shift(),
+				responseFormat = mimeType.split(/[\/\+]/).pop(),
+				ctor = constructors[responseFormat];
+		
+			if (!ctor) {
+				throw { 
+					name: "Constructor Not Found",
+					message: "No constructor exists for type " + contentType
+				};
+			}
+		
+			return ctor(spec);
+		};
 
 	HATEOAS_CONSOLE.responseParsers.linkFinderFactory = function LinkFinderFactory() {
 		
