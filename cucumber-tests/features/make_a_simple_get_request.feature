ea11cda5dc8ec54f9f5b01d77cd3eb5068648c68
Matthew Butt <matthew.butt@7digital.com>
2011-09-16 16:34:47 +0100

    Refactoring initial cucumber test

--- cucumber-tests/features/make_a_simple_get_request.feature
+++ cucumber-tests/features/make_a_simple_get_request.feature
@@ -2,7 +2,8 @@
 Feature: Make a simple GET request
 
   Background:
-    Given I open the hateoas console
+    Given I have a browser open
+	And I open the hateoas console
     And I enter http://www.google.com/ in the address box
 	And I select GET from the verb selector
 	And I click Go
--- cucumber-tests/features/step_definitions/browser.rb
+++ cucumber-tests/features/step_definitions/browser.rb
@@ -1,23 +1,27 @@
 require 'watir-webdriver'
 
+Given /^I have a browser open$/ do
+  @browser ||= Watir::Browser.new :firefox
+end
+
 Given /^I open the hateoas console$/ do
-  browser.goto "http://hateoas-console.local/"
+  @browser.goto "http://hateoas-console.local/"
 end
 
 Given /^I enter (\S*) in the address box$/ do |address|
-  @@address = address
-  browser.text_field(:name => 'url').set(address)
+  @address = address
+  @browser.text_field(:name => 'url').set(address)
 end
 
 Given /^I select (\S*) from the verb selector$/ do |verb|
-  @@verb = verb
-  browser.select_list(:name => 'verb').select verb
+  @verb = verb
+  @browser.select_list(:name => 'verb').select verb
 end
 
 Given /^I click Go$/ do
-  browser.button(:value => 'Go').click
+  @browser.button(:value => 'Go').click
 end
 
 Then /^my request is logged$/ do
-  browser.pre(:id => 'request').text.include? '#{@@verb} #{@@address}'
+  @browser.pre(:id => 'request').text.include? '#{@verb} #{@address}'
 end
