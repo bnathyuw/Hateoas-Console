a6942930c1e291709901d1221100199fddeb2c82
Matthew Butt <matthew.butt@7digital.com>
2011-10-03 15:55:31 +0100

    Created PageView view and set up initial spec for it

---
+++ unit-tests/spec/views/PageViewSpec.js
@@ -0,0 +1,16 @@
+﻿/*global describe: false, beforeEach: false, HATEOAS_CONSOLE: false, it: false, expect: false */
+
+describe("PageView", function () {
+	"use strict";
+	
+	var pageView;
+	
+	beforeEach(function () {
+		pageView = new HATEOAS_CONSOLE.views.PageView();
+	});
+	
+	it("should have a method initialize", function () {
+		expect(pageView.initialize).toBeTruthy();
+	});
+	
+});
