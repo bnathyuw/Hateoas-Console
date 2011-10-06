0db751c60c56b0fb11b768d11ecc40d7cc51d956
Matthew Butt <matthew.butt@7digital.com>
2011-10-06 12:14:03 +0100

    Got rid of PageView and associated spec

--- app/scripts/views/PageView.js
+++
@@ -1,7 +0,0 @@
-﻿/*global HATEOAS_CONSOLE: false, Backbone: false, $: false */
-
-HATEOAS_CONSOLE.namespace("HATEOAS_CONSOLE.views");
-
-HATEOAS_CONSOLE.views.PageView = Backbone.View.extend({
-	el: $("body")
-});
