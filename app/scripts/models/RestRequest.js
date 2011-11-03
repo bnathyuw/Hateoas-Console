3477a6b66060d718f68a5ee9c9ef5352230c49cd
Matthew Butt <matthew.butt@7digital.com>
2011-11-03 15:47:01 +0000

    Making namespacing consistent

--- app/scripts/models/RestRequest.js
+++ app/scripts/models/RestRequest.js
@@ -1,6 +1,6 @@
 /*global HATEOAS_CONSOLE: false, Backbone: false */
 
-HATEOAS_CONSOLE.namespace("HATEOAS_CONSOLE.models");
+HATEOAS_CONSOLE.namespace("models");
 
 HATEOAS_CONSOLE.models.RestRequest = (function () {
 	"use strict";
